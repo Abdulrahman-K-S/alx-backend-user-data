b6a4c82c93e56b7cf3ddf17342524ca1f5760676
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-30 18:02:27 +0300

    (change): Fix localhost

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,7 +63,7 @@ def get_db() -> connection.MySQLConnection:
     """
     username = environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
     password = environ.get('PERSONAL_DATA_DB_PASSWORD', '')
-    host = environ.get('PERSONAL_DATA_DB_HOST', 'local_host')
+    host = environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
     db_name = environ.get('PERSONAL_DATA_DB_NAME')
 
     connector = connection.MySQLConnection(
