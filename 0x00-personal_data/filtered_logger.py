72c7a3cb70cf1d9fa42cde2c378580b19095a53e
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-30 18:00:50 +0300

    (change): Implicit return

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,7 +53,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> connection.MySQLConnection:
     """get_db
 
     Connects to the secure database and returns the connector.
