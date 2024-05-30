0d6fcc14af787ac41ee7ff0932a1e9d748b941b1
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-05-30 18:14:49 +0300

    (feat): Add task "6. Check valid password"
    Implement an `is_valid` function that expects 2 arguments and returns a `boolean`.
    
    Arguments:
    
    - `hashed_password`: `bytes` type
    - `password`: string type
    Use `bcrypt` to validate that the provided password matches the hashed password.
    
    ```py
    bob@dylan:~$ cat main.py
    #!/usr/bin/env python3
    """
    Main file
    """
    
    hash_password = __import__('encrypt_password').hash_password
    is_valid = __import__('encrypt_password').is_valid
    
    password = "MyAmazingPassw0rd"
    encrypted_password = hash_password(password)
    print(encrypted_password)
    print(is_valid(encrypted_password, password))
    
    bob@dylan:~$
    bob@dylan:~$ ./main.py
    b'$2b$12$Fnjf6ew.oPZtVksngJjh1.vYCnxRjPm2yt18kw6AuprMRpmhJVxJO'
    True
    bob@dylan:~$
    ```
    
    File:
    - encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-Task 5. Encrypting passwords
+Task 5. Encrypting passwords & 6. Check valid password
 
-Encrypt the passwords
+Encrypt the passwords & check if it's the hashed password or not.
 """
 
 import bcrypt
@@ -20,3 +20,20 @@ def hash_password(password: str) -> bytes:
         (bytes): The hashed password in bytes.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid
+
+    Checks whether the hashed password is the same as the
+    passed password and returns a bool accordingly.
+
+    Arguments:
+        hashed_password (bytes):
+        password (str):
+
+    Return:
+        (bool): [True: if the hashed password is equal to password/
+                 False: if the hashed password isn't equal to password]
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
