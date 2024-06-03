adc2834c8313e9d1985bf25deee6087262a0c3c4
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-03 16:59:29 +0300

    (feat): Add task "1. Error handler: Unauthorized"
    What the HTTP status code for a request unauthorized? `401` of course!
    
    Edit `api/v1/app.py`:
    
    - Add a new error handler for this status code, the response must be:
     - a JSON: `{"error": "Unauthorized"}`
     - status code `401`
     - you must use `jsonify` from Flask
    For testing this new error handler, add a new endpoint in `api/v1/views/index.py`:
    
    - Route: `GET /api/v1/unauthorized`
    - This endpoint must raise a 401 error by using `abort` - [Custom Error Pages](https://intranet.alxswe.com/rltoken/RH0gY_XQuSB75Q-JbI-fdg)
    By calling `abort(401)`, the error handler for 401 will be executed.
    
    In the first terminal:
    
    ```
    bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 python3 -m api.v1.app
     * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
    ....
    ```
    
    In a second terminal:
    
    ```py
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/unauthorized"
    {
      "error": "Unauthorized"
    }
    bob@dylan:~$
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/unauthorized" -vvv
    *   Trying 0.0.0.0...
    * TCP_NODELAY set
    * Connected to 0.0.0.0 (127.0.0.1) port 5000 (#0)
    > GET /api/v1/unauthorized HTTP/1.1
    > Host: 0.0.0.0:5000
    > User-Agent: curl/7.54.0
    > Accept: */*
    >
    * HTTP 1.0, assume close after body
    < HTTP/1.0 401 UNAUTHORIZED
    < Content-Type: application/json
    < Content-Length: 30
    < Server: Werkzeug/0.12.1 Python/3.4.3
    < Date: Sun, 24 Sep 2017 22:50:40 GMT
    <
    {
      "error": "Unauthorized"
    }
    * Closing connection 0
    bob@dylan:~$
    ```
    
    Files:
    - api/v1/app.py
    - api/v1/views/index.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,14 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+def request_unauthorized(error) -> str:
+    """request_unauthorized
+
+    The HTTP request is unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
