bba9c9a7f4f1495ea3801d9f7c80a30c64df73ea
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-03 17:42:13 +0300

    (feat): Add task "5. Request validation!"
    Now you will validate all requests to secure the API:
    
    Update the method def authorization_header(self, request=None) -> str: in api/v1/auth/auth.py:
    
    - If request is None, returns None
    - If request doesn’t contain the header key Authorization, returns None
    - Otherwise, return the value of the header request Authorization
    
    Update the file api/v1/app.py:
    
    - Create a variable auth initialized to None after the CORS definition
    - Based on the environment variable AUTH_TYPE, load and assign the right instance of authentication to auth
     - if auth:
      - import Auth from api.v1.auth.auth
      - create an instance of Auth and assign it to the variable auth
    
    Now the biggest piece is the filtering of each request. For that you will use the Flask method before_request
    
    - Add a method in api/v1/app.py to handler before_request
     - if auth is None, do nothing
     - if request.path is not part of this list ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/'], do nothing - you must use the method require_auth from the auth instance
     - if auth.authorization_header(request) returns None, raise the error 401 - you must use abort
     - if auth.current_user(request) returns None, raise the error 403 - you must use abort
    In the first terminal:
    
    ```
    bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 AUTH_TYPE=auth python3 -m api.v1.app
     * Running on http://0.0.0.0:5000/ (Press CTRL+C to quit)
    ....
    ```
    
    In a second terminal:
    
    ```
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/status"
    {
      "status": "OK"
    }
    bob@dylan:~$
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/status/"
    {
      "status": "OK"
    }
    bob@dylan:~$
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/users"
    {
      "error": "Unauthorized"
    }
    bob@dylan:~$
    bob@dylan:~$ curl "http://0.0.0.0:5000/api/v1/users" -H "Authorization: Test"
    {
      "error": "Forbidden"
    }
    bob@dylan:~$
    ```
    
    Files:
    - api/v1/app.py
    - api/v1/auth/auth.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,13 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 
 @app.errorhandler(404)
@@ -39,7 +41,29 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def authenticate():
+    """authenticate
+
+    Authenticates the user before processing a request.
+    """
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/'
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header() is None:
+                abort(401)
+            if auth.current_user() is None:
+                abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
+    auth = getenv("AUTH_TYPE")
+    if auth:
+        auth = Auth()
     app.run(host=host, port=port)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,7 +31,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """authorization_header
         """
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user
