12aac1620dea3bb34baf6bc82444d72f7674fddb
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-04 15:12:10 +0300

    (feat): Add task "10. Basic - User object"
    Add the method def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'): in the class BasicAuth that returns the User instance based on his email and password.
    
    - Return None if user_email is None or not a string
    - Return None if user_pwd is None or not a string
    - Return None if your database (file) doesn’t contain any User instance with email equal to user_email - you should use the class method search of the User to lookup the list of users based on their email. Don’t forget to test all cases: “what if there is no user in DB?”, etc.
    - Return None if user_pwd is not the password of the User instance found - you must use the method is_valid_password of User
    - Otherwise, return the User instance
    
    ```py
    bob@dylan:~$ cat main_5.py
    #!/usr/bin/env python3
    """ Main 5
    """
    import uuid
    from api.v1.auth.basic_auth import BasicAuth
    from models.user import User
    
    """ Create a user test """
    user_email = str(uuid.uuid4())
    user_clear_pwd = str(uuid.uuid4())
    user = User()
    user.email = user_email
    user.first_name = "Bob"
    user.last_name = "Dylan"
    user.password = user_clear_pwd
    print("New user: {}".format(user.display_name()))
    user.save()
    
    """ Retreive this user via the class BasicAuth """
    
    a = BasicAuth()
    
    u = a.user_object_from_credentials(None, None)
    print(u.display_name() if u is not None else "None")
    
    u = a.user_object_from_credentials(89, 98)
    print(u.display_name() if u is not None else "None")
    
    u = a.user_object_from_credentials("email@notfound.com", "pwd")
    print(u.display_name() if u is not None else "None")
    
    u = a.user_object_from_credentials(user_email, "pwd")
    print(u.display_name() if u is not None else "None")
    
    u = a.user_object_from_credentials(user_email, user_clear_pwd)
    print(u.display_name() if u is not None else "None")
    
    bob@dylan:~$
    bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 ./main_5.py
    New user: Bob Dylan
    None
    None
    None
    None
    Bob Dylan
    bob@dylan:~$
    ```
    
    Files:
    - api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,9 @@ The BasicAuth class
 """
 
 from api.v1.auth.auth import Auth
+from models.user import User
 from base64 import b64decode
-from typing import Tuple
+from typing import Tuple, TypeVar
 
 
 class BasicAuth(Auth):
@@ -58,3 +59,23 @@ class BasicAuth(Auth):
             return (None, None)
         return decoded_base64_authorization_header.split(":", 1)[0], \
             decoded_base64_authorization_header.split(":", 1)[1]
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials
+
+        Returns the user instance based on the email & password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
