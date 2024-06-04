c495918a2345ce5c1ac1dcad3a160684e43bf1d3
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-04 14:44:03 +0300

    (feat): Add task "7. Basic - Base64 part"
    Add the method def extract_base64_authorization_header(self, authorization_header: str) -> str: in the class BasicAuth that returns the Base64 part of the Authorization header for a Basic Authentication:
    
    - Return None if authorization_header is None
    - Return None if authorization_header is not a string
    - Return None if authorization_header doesn’t start by Basic (with a space at the end)
    - Otherwise, return the value after Basic (after the space)
    - You can assume authorization_header contains only one Basic
    
    ```py
    bob@dylan:~$ cat main_2.py
    #!/usr/bin/env python3
    """ Main 2
    """
    from api.v1.auth.basic_auth import BasicAuth
    
    a = BasicAuth()
    
    print(a.extract_base64_authorization_header(None))
    print(a.extract_base64_authorization_header(89))
    print(a.extract_base64_authorization_header("Holberton School"))
    print(a.extract_base64_authorization_header("Basic Holberton"))
    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
    print(a.extract_base64_authorization_header("Basic1234"))
    
    bob@dylan:~$
    bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 ./main_2.py
    None
    None
    None
    Holberton
    SG9sYmVydG9u
    SG9sYmVydG9uIFNjaG9vbA==
    None
    bob@dylan:~$
    ```
    
    Files:
    - api/v1/auth/basic.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,3 +13,15 @@ class BasicAuth(Auth):
 
     An imporved API authentication using the Auth class.
     """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """extract_base64_authorization_header
+
+        Returns the Base64 part of the authorization header.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if "Basic " not in authorization_header:
+            return None
+        return authorization_header.split('Basic ', 1)[1]
