24b5bb4ac8b04b7b721a3e9597e680a1c3216f9b
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2024-06-06 19:47:32 +0300

    (feat): Add task "2. Create a session"
    Update SessionAuth class:
    
    - Create a class attribute user_id_by_session_id initialized by an empty dictionary
    - Create an instance method def create_session(self, user_id: str = None) -> str: that creates a Session ID for a user_id:
     - Return None if user_id is None
     - Return None if user_id is not a string
     - Otherwise:
      - Generate a Session ID using uuid module and uuid4() like id in Base
      - Use this Session ID as key of the dictionary user_id_by_session_id - the value for this key must be user_id
      - Return the Session ID
     - The same user_id can have multiple Session ID - indeed, the user_id is the value in the dictionary user_id_by_session_id
    
    Now you an “in-memory” Session ID storing. You will be able to retrieve an User id based on a Session ID.
    
    ```py
    bob@dylan:~$ cat  main_1.py
    #!/usr/bin/env python3
    """ Main 1
    """
    from api.v1.auth.session_auth import SessionAuth
    
    sa = SessionAuth()
    
    print("{}: {}".format(type(sa.user_id_by_session_id), sa.user_id_by_session_id))
    
    user_id = None
    session = sa.create_session(user_id)
    print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
    
    user_id = 89
    session = sa.create_session(user_id)
    print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
    
    user_id = "abcde"
    session = sa.create_session(user_id)
    print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
    
    user_id = "fghij"
    session = sa.create_session(user_id)
    print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
    
    user_id = "abcde"
    session = sa.create_session(user_id)
    print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
    
    bob@dylan:~$
    bob@dylan:~$ API_HOST=0.0.0.0 API_PORT=5000 AUTH_TYPE=session_auth ./main_1.py
    <class 'dict'>: {}
    None => None: {}
    89 => None: {}
    abcde => 61997a1b-3f8a-4b0f-87f6-19d5cafee63f: {'61997a1b-3f8a-4b0f-87f6-19d5cafee63f': 'abcde'}
    fghij => 69e45c25-ec89-4563-86ab-bc192dcc3b4f: {'61997a1b-3f8a-4b0f-87f6-19d5cafee63f': 'abcde', '69e45c25-ec89-4563-86ab-bc192dcc3b4f': 'fghij'}
    abcde => 02079cb4-6847-48aa-924e-0514d82a43f4: {'61997a1b-3f8a-4b0f-87f6-19d5cafee63f': 'abcde', '02079cb4-6847-48aa-924e-0514d82a43f4': 'abcde', '69e45c25-ec89-4563-86ab-bc192dcc3b4f': 'fghij'}
    bob@dylan:~$
    ```
    
    Files:
    - api/v1/auth/session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,22 @@
 The session authentication class
 """
 
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """SessionAuth
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create_session
+
+        Creates a session id for a user id.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
